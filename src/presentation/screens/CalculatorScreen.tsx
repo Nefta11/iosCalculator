15b26742aef211e5e745e37161ccbbbb5474897e
Nefta11 <neftaliarturohernandez@gmail.com>
2025-04-02 22:12:36 -0600

    Agregar estilos al componente CalculatorScreen y actualizar dependencias en package.json

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const CalculatorScreen = () => {
     return (
-        <View>
-            <Text>Calculator Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Calculator</Text>
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
 export default CalculatorScreen;

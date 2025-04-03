bf6a71bab097dcbb5a7379b088f5c7df5bbb76e1
Nefta11 <neftaliarturohernandez@gmail.com>
2025-04-02 22:24:51 -0600

    Agregar archivo de tema de aplicación con colores y estilos base

---
+++ src/config/theme/app-theme.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const colors = {
+    darkGray: '#2D2D2D',
+    lightGray: '#9B9B9B',
+    orange: '#FF8C00',
+
+    textPrimary: 'white',
+    textSecondary: '#666666',
+    background: '#000000',
+};
+
+export const styles = StyleSheet.create({
+});
